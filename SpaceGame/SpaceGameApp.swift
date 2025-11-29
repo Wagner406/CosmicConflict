94206eabb64e3aaf04fc846ca96ba60e5db2a2a5
alexanderwagner <wagner.alexander2@web.de>
2025-11-29 23:27:56 +0100

    Initial Commit

---
+++ SpaceGame/SpaceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceGameApp.swift
+//  SpaceGame
+//
+//  Created by Alexander Wagner on 29.11.25.
+//
+
+import SwiftUI
+
+@main
+struct SpaceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpaceGameTests/SpaceGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceGameTests.swift
+//  SpaceGameTests
+//
+//  Created by Alexander Wagner on 29.11.25.
+//
+
+import Testing
+@testable import SpaceGame
+
+struct SpaceGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
